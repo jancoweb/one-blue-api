2566418e4a81bfd43488678a913658620c6a349f
Joao Coelho <jancoweb@outlook.com>
2022-07-20 16:06:36 -0300

    correções

--- src/Controladores/Usuario/visualizarCadastro.js
+++ src/Controladores/Usuario/visualizarCadastro.js
@@ -1,6 +1,21 @@
+const conexao = require('../../conexao');
+
 const visualizarCadastro = async (req, res)=>{
   const { usuario } = req;
-  return res.status(200).json(usuario);
+
+  try {
+    const query = 'select * from pensamentos where usuario_id = $1';
+    let posts = await conexao.query(query, [usuario.id]);
+
+    if(posts.rowCount === 0) posts = '';
+
+    const publicacoes = posts.rows
+    
+    return res.status(200).json({usuario, publicacoes});
+  } catch (e) {
+    return res.send(e.message)
+  }
+
 };
 
 module.exports = visualizarCadastro;
